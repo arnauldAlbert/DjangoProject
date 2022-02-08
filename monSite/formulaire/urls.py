4daaba52d1a7eaee328f1d1984f7fa379bb40dbb
Arnauld ALBERT <arnauld.albert@uha.fr>
2022-02-08 10:54:28 +0100

    mise à jour du site avec le Update Delet du CRUD et la vue all. ajout aussi des CSS en static

--- monSite/formulaire/urls.py
+++ monSite/formulaire/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('formulaire', views.formulaire, name='formulaire'),
-    path('traitement/', views.traitement, name='affiche'),
+    path('traitement/', views.traitement, name='traitement'),
+    path('<int:id>/traitement/', views.traitementUpdate, name='traitement_update'),
     path('', views.index, name='index'),
+    path('<int:id>/update/',views.update,name="update"),
+    path('<int:id>/delete/',views.delete,name="delete"),
 ]
--- monSite/formulaire/views.py
+++ monSite/formulaire/views.py
@@ -19,15 +19,37 @@ def formulaire(request):
     return render(request, 'formulaire/name.html', {'form': form})
 
 
-def traitement(request):
+def traitementUpdate(request, id):
+    pForm = PersonForm2(request.POST)
+    if pForm.is_valid():
+        person = pForm.save(commit=False)
+        person.id=id
+        p = pForm.save()
+        # return render(request,'formulaire/affiche.html', {'person': person})
+        """ permet de faire la redirection vers la page d'accueil """
+        return HttpResponseRedirect("/formulaire/")
+    else:
+        return render(request, 'formulaire/name.html', {'form': pForm})
 
+def traitement(request):
     pForm = PersonForm2(request.POST)
     if pForm.is_valid():
         person = pForm.save()
-        return render(request,'formulaire/affiche.html', {'person': person})
+        """ permet de faire la redirection vers la page d'accueil"""
+        return HttpResponseRedirect("/formulaire/")
     else:
         return render(request, 'formulaire/name.html', {'form': pForm})
 
 def index(request):
     liste = list(models.Person.objects.all())
     return render(request,'formulaire/index.html',{'liste': liste})
+
+def update(request, id):
+    p = models.Person.objects.get(pk=id)
+    pForm = PersonForm2(None, instance = p)
+    return render(request, 'formulaire/update.html', {'form': pForm, 'id' : id})
+
+def delete(request, id):
+    p = models.Person.objects.get(pk=id)
+    p.delete()
+    return HttpResponseRedirect("/formulaire/")
